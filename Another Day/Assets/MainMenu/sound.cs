8533db3a5583f4019942b5f6530beb1f7b19333a
jusilll <Keeperjustin07@gmail.com>
2025-11-21 10:29:15 +0100

    setting werkt grotendeels

---
+++ Another Day/Assets/MainMenu/sound.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class sound : MonoBehaviour {
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
