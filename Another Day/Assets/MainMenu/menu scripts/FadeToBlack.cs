ecdfd814a8bbfa5c37b8d35bf9061dc5a1c6d2cd
jusilll <Keeperjustin07@gmail.com>
2025-12-02 09:57:35 +0100

    fade  systeem werkt
    Introduced a new Unlit Transparent Color shader, FadeToBlack material, and FadeToBlack script to enable fade-to-black transitions in the main menu. Updated the scene to include a quad with the fade effect and adjusted the timer starting value. Also modified XR package settings feature list.

---
+++ Another Day/Assets/MainMenu/menu scripts/FadeToBlack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class FadeToBlack : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool FadeOnLoose;
+    public float FadeDuration = 2;
+    public Color FadeColor;
+    private Renderer rend;
+
+    void Update()
+    {
+        rend = GetComponent<Renderer>();
+        if (FadeOnLoose )
+            fadeOut();
+    }
+    public void fade(float alphaIn, float alphaOut)
+    {
+        StartCoroutine(fadeRoutine(alphaIn, alphaOut));
+    }
+    public void fadeIn()
+    {
+        fade(1, 0);
+    }
+    public void fadeOut()
+    {
+        fade(0, 1);
+    }
+    public IEnumerator fadeRoutine(float alphaIn, float alphaOut)
+    {
+        float timer = 0;
+        while (timer < FadeDuration)
+        {
+            Color newcolor = FadeColor;
+            newcolor.a = Mathf.Lerp(alphaIn, alphaOut, timer/FadeDuration);
+            rend.material.SetColor("_Color", newcolor);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
