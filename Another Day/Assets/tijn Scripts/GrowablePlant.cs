ed269fa584884b829ae8eab5f29549a3353a1751
TijnDN <tijnden@gmail.com>
2025-12-10 14:41:15 +0100

    plant kan groeien

---
+++ Another Day/Assets/tijn Scripts/GrowablePlant.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrowablePlant : MonoBehaviour
+{
+    [SerializeField] GameObject bigPlantPrefab;
+    [SerializeField] float waterTimeNeeded = 3f;
+
+    float timer = 0f;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("WateringCan"))
+        {
+            timer += Time.deltaTime;
+            if (timer >= waterTimeNeeded)
+            {
+                GameObject bigPlant = Instantiate(bigPlantPrefab, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("WateringCan"))
+            timer = 0f;
+    }
+}
